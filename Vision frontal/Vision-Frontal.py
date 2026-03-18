e4e794275b02ac486b5e961c8139e6a841be116c
Jared AlPa <jaredaldanapalacios@gmail.com>
2026-03-18 13:36:02 -0600

    Refactor color thresholds and improve initialization

--- Vision frontal/Vision-Frontal.py
+++ Vision frontal/Vision-Frontal.py
@@ -1,6 +1,6 @@
 '''
  * file Vision-Frontal.py
- * author Jared Aldana Palacios 
+ * author Jared Aldana Palacios
  * brief Source file for the OpenMV
  * date 2026-02-12
 '''
@@ -22,34 +22,44 @@ MIN_CLOSE_AREA = 100
 MIN_FAR_AREA = 4
 MON_PIXELS = 3
 
-# UART COMMUNICATION 
+# UART COMMUNICATION
 uart = UART(3, 115200, timeout_char=0)
 uart.init(115200, bits=8, parity=None, stop=1)
 
 # THRESHOLDS
-BALL_COLOR_THRESHOLD   = (40, 100, 30, 127, 20, 127)
-THRESHOLD_YELLOW_GOAL = (50, 73, -55, 48, 8, 28)
-THRESHOLD_BLUE_GOAL = (35, 100, -128, -10, -128, -10)
+THRESHOLD_BALL_1 = (81, 34, 61, 29, 0, 39)
+THRESHOLD_BALL_2 = (81, 34, 61, 29, 0, 39)
+THRESHOLD_BALL_3 = (81, 34, 61, 29, 0, 39)
 
-# IMAGE CENTER 
+THRESHOLD_YELLOW_GOAL_1 = (35, 55, 3, 22, 15, 23)
+THRESHOLD_YELLOW_GOAL_2 = (48, 82, -10, 12, 15, 62)
+THRESHOLD_YELLOW_GOAL_3 = (48, 74, 5, 28, 17, 33)
+
+THRESHOLD_BLUE_GOAL_1 = (14, 13, -3, 12, -11, -8)
+THRESHOLD_BLUE_GOAL_2 = (9,  17, -5, 18, -14, -3)
+THRESHOLD_BLUE_GOAL_3 = (13, 19, 8, 5, -18, -3)
+
+BALL_MIN_CIRCULARITY = 0.3
+
+# IMAGE CENTER
 X_CENTER = CAMERA_WIDTH // 2
 Y_CENTER = CAMERA_HEIGHT // 2
 
-# INITIALIZATION 
+# INITIALIZATION
 def initialize_open():
     sensor.reset()
     sensor.set_pixformat(sensor.RGB565)
     sensor.set_framesize(sensor.QVGA)
-    sensor.skip_frames(time=3000)
+    sensor.skip_frames(time=0)
 
     sensor.set_auto_gain(False)
     sensor.set_gainceiling(16)
     sensor.set_auto_whitebal(False)
     sensor.set_auto_exposure(False, exposure_us=45000)
 
-    sensor.set_brightness(-1)
-    sensor.set_contrast(-4)
-    sensor.set_saturation(-6)
+    sensor.set_brightness(-5)
+    sensor.set_contrast(-5)
+    sensor.set_saturation(-5)
 
     sensor.set_hmirror(True)
     sensor.set_transpose(True)
@@ -73,13 +83,14 @@ def main():
     while True:
         clock.tick()
         img = sensor.snapshot()
+        #img.draw_rectangle((0, 0, 25, img.height()), color=(0, 0, 0), fill=True)
 
         # BALL DETECTION
         distance_ball = 0
         angle_ball = 0
 
         blobs = img.find_blobs(
-            [BALL_COLOR_THRESHOLD],
+            [THRESHOLD_BALL_1, THRESHOLD_BALL_2, THRESHOLD_BALL_3],
             pixels_threshold=MON_PIXELS,
             area_threshold=MIN_FAR_AREA,
             merge=True
@@ -99,10 +110,10 @@ def main():
                 #circularity= blob.compactness() # compactness = (perímeter²) / (4π × área) Values between 1 and ∞, being 1 a perfect circle
 
                 # Near ball (Larger and more circular blobs)
-                if area >= MIN_CLOSE_AREA and circularity > 0.7:
+                if area >= MIN_CLOSE_AREA and circularity > BALL_MIN_CIRCULARITY:
                     valid_blob = True
 
-                # Far ball (Almost a point/caution cables) 
+                # Far ball (Almost a point/caution cables)
                 elif MIN_FAR_AREA <= area < MIN_CLOSE_AREA:
                     valid_blob = True
 
@@ -126,7 +137,7 @@ def main():
         angle_yellow_goal = 0
 
         blobs = img.find_blobs(
-            [THRESHOLD_YELLOW_GOAL],
+            [THRESHOLD_YELLOW_GOAL_1, THRESHOLD_YELLOW_GOAL_2, THRESHOLD_YELLOW_GOAL_3],
             pixels_threshold=200,
             area_threshold=800,
             merge=True
@@ -144,7 +155,7 @@ def main():
         angle_blue_goal = 0
 
         blobs = img.find_blobs(
-            [THRESHOLD_BLUE_GOAL],
+            [THRESHOLD_BLUE_GOAL_1, THRESHOLD_BLUE_GOAL_2, THRESHOLD_BLUE_GOAL_3],
             pixels_threshold=200,
             area_threshold=800,
             merge=True
